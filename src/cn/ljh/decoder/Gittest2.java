f4302e7b2082e365ed33c504119f86ae2f55c04a
3013779515 <3013779515@qq.com>
2017-04-24 08:11:12 +0800

    patch test01

---
+++ src/cn/ljh/decoder/Gittest2.java
@@ -0,0 +1,32 @@
+package cn.ljh.decoder;
+
+public class Gittest2 {
+	private int num;
+	private String name;
+	private String demo;
+	
+
+	
+	public String getDemo() {
+		return demo;
+	}
+
+	public void setDemo(String demo) {
+		this.demo = demo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+
+
+}
